12a882ff877b9da907846764322984f307702217
Karolis <karolis.zuk@gmail.com>
2017-12-13 11:52:58 +0200

    Semantic-UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Container} from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import AboutPage from './components/pages/AboutPage';
 import NotesPage from './components/pages/NotesPage';
 import ContactPage from './components/pages/ContactPage';
 
+
 const App = () => 
-<div>
+<Container>
 <Route path="/" exact component={HomePage} />
 <Route path="/about" exact component={AboutPage} />
 <Route path="/notes" exact component={NotesPage} />
 <Route path="/contact" exact component={ContactPage} />
-</div>;
+</ Container>;
 
 export default App;
